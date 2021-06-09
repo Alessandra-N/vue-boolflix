cf7b5f97756ac9126387d822617337cb1c23aeb0
Alessandra-N <nardella.alessandra1987@gmail.com>
2021-06-09 17:54:47 +0200

    select con la lista dei generi

--- Milestone_5/assets/js/main.js
+++ Milestone_5/assets/js/main.js
@@ -3,20 +3,24 @@ const app = new Vue  ({
     el: "#app",
 
     data: {
-        url: "https://api.themoviedb.org/3/search/movie?api_key=721dbf1e3d02ede41edd01ebb9dda0b6",
+        movieUrl: "https://api.themoviedb.org/3/search/movie?api_key=721dbf1e3d02ede41edd01ebb9dda0b6",
         seriesUrl: "https://api.themoviedb.org/3/search/tv?api_key=721dbf1e3d02ede41edd01ebb9dda0b6",
+        genresUrl: "https://api.themoviedb.org/3/genre/movie/list?api_key=721dbf1e3d02ede41edd01ebb9dda0b6",
         searchedMovie: "",
         movies: [],
         tvSeries: [],
+        genres: [],
     },
 
     methods: {
 
         searchMovies() {
             axios
-            .get(this.url + "&query=" + this.searchedMovie.replace(/ /g, '+'))
+            .get(this.movieUrl + "&query=" + this.searchedMovie.replace(/ /g, '+'))
             .then(response => {
                 this.movies = response.data.results;
+                console.log(response.data);
+               
             })
             .catch(e => {
             console.error(e);
@@ -31,8 +35,20 @@ const app = new Vue  ({
             console.error(e);
             })
             
+            /* https://api.themoviedb.org/3/movie/{movie_id}&language=en-US */
         }
         
     },
+
+    mounted () {
+        axios
+        .get(this.genresUrl)
+        .then(response => {
+            this.genres = response.data.genres
+        })
+        .catch(e => {
+            console.error(e);
+        })
+    }
 
 })
